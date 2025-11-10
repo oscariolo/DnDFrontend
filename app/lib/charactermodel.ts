ce05f33ff4fb6996b5168a916a80c591139d78e7
Oscariolo <oscariolopinop21@gmail.com>
2025-11-10 18:52:56 -0500

    ADD-Added class choosing and save with localstorage

---
+++ app/lib/charactermodel.ts
@@ -0,0 +1,12 @@
+import { Attributes } from "./classmodel";
+
+interface CustomCharacter{
+    name: string;
+    class: string;
+    currentAttributes: Attributes;
+    race: string;
+    description: string;
+    skills: string[];
+    imgsrc?: string;
+    startItems?: string[];
+}
