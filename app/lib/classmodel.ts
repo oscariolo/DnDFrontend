210b3377a6679c5b6f3f36d2cb28c68d96f2a4f4
Oscariolo <oscariolopinop21@gmail.com>
2025-11-09 22:46:03 -0500

    ADD-Added lib folder for types and interfaces, added the classmodel

---
+++ app/lib/classmodel.ts
@@ -0,0 +1,14 @@
+export interface Attributes{
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
+}
+
+export interface CharacterClass{
+  name: string;
+  description: string;
+  attributes: Attributes;
+}
