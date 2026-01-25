fb6075767d6bea4eef92a5fa81e58e42c149e70a
Oscariolo <oscariolopinop21@gmail.com>
2026-01-25 00:11:02 -0500

    MOD-Environment public backend

--- app/lib/services/authService.ts
+++ app/lib/services/authService.ts
@@ -1,4 +1,4 @@
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080';
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost';
 
 export interface LoginRequest {
   username: string;
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev',process.env.NEXT_PUBLIC_BACKEND_URL|| 'http://localhost'],
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
